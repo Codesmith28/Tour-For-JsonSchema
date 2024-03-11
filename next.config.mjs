33f6e8e936de6bc466cb6fc1f8ecfb9fcee1adc9
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-03-11 11:47:51 +0530

    add workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		config.module.rules.push({
+			test: /\.md$/,
+			// This is the asset module.
+			type: "asset/source",
+		});
+		return config;
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	output: process.env.HOST === "Vercel" ? "" : "export",
+	basePath: process.env.HOST === "Vercel" ? "" : "/Tour-of-Json-Schema",
+};
 
 export default nextConfig;
